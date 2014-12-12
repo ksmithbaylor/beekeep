9d926cde7b2cb603e54b57ac369830c177e6f3fa
Kevin Smith <ksmithbaylor@gmail.com>
2014-12-11 19:33:40 -0500

    added new install and build config

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,66 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var bower = require('bower');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var sh = require('shelljs');
+var gulp       = require('gulp'),
+    sourcemaps = require('gulp-sourcemaps'),
+    browserify = require('browserify'),
+    bower      = require('bower'),
+    sass       = require('gulp-sass'),
+    minifyCss  = require('gulp-minify-css'),
+    rename     = require('gulp-rename'),
+    plumber    = require('gulp-plumber'),
+    tap        = require('gulp-tap'),
+    webserver  = require('gulp-webserver'),
+    _          = require('lodash');
+
+var inputDir  = './app/';
+var outputDir = './www/';
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+    sass       : [inputDir + 'style/app.scss'],
+    js         : [inputDir + '**/*.js'],
+    jsEntry    : inputDir  + 'app.js',
+    html       : [inputDir + '**/*.html'],
+    assets     : ['./assets/**/*'],
+    htmlDest   : outputDir,
+    libDest    : outputDir + 'lib/',
+    assetsDest : outputDir + 'assets/'
 };
 
-gulp.task('default', ['sass']);
+gulp.task('compile', ['assets', 'html', 'sass', 'js'])
+gulp.task('default', ['compile', 'watch']);
+
+gulp.task('js', function () {
+    function bundler(file) {
+        file.contents = browserify(file.path).bundle();
+    }
+
+    gulp.src(paths.jsEntry, {read: false})
+        .pipe(plumber())
+        .pipe(tap(bundler))
+        .pipe(gulp.dest(outputDir + 'js/'));
+});
 
 gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
+    gulp.src(paths.sass)
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            errLogToConsole: true
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(minifyCss())
+        .pipe(rename('app.bundle.css'))
+        .pipe(gulp.dest(outputDir + 'css/'))
+        .on('end', done);
 });
 
-gulp.task('watch', function() {
-  gulp.watch(paths.sass, ['sass']);
-});
-
-gulp.task('install', ['git-check'], function() {
-  return bower.commands.install()
-    .on('log', function(data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+_.each(['html', 'assets'], function(name) {
+    gulp.task(name, function() {
+        return gulp.src(paths[name]).pipe(gulp.dest(paths[name + 'Dest']));
     });
 });
 
-gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-    );
-    process.exit(1);
-  }
-  done();
+gulp.task('watch', function() {
+    var nonWatchPaths = ['jsEntry', 'htmlDest', 'libDest', 'assetsDest'];
+    _.forOwn(paths, function(path, name) {
+        if (_.contains(nonWatchPaths, name)) return;
+        gulp.watch(path, [name]);
+    });
 });
