3cfdcbac0705164c6df03469eb81591956750c4a
Kevin Smith <ksmithbaylor@gmail.com>
2015-02-14 09:05:19 -0500

    got rid of unneeded header module

--- app/modules/header/index.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports = require('angular').module('beekeep.header', ['ionic', 'beekeep.scanner']);
-
-module.exports.controller('HeaderController', require('./HeaderController'));
--- app/modules/index.js
+++ app/modules/index.js
@@ -3,7 +3,6 @@
 var _ = require('lodash');
 
 module.exports = _.map([
-    require('./header'),
     require('./home'),
     require('./scanner'),
     require('./overview'),
