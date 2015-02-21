3a772183e1cef6ee88c1537ed8ebea2d2be70550
Kevin Smith <ksmithbaylor@gmail.com>
2015-02-20 20:33:23 -0500

    got rid of old demo code in home controller

--- app/modules/home/HomeController.js
+++ app/modules/home/HomeController.js
@@ -1,10 +1,5 @@
 'use strict';
 
 module.exports = function($scope, $rootScope) {
-    $scope.currentScanResult = 'initial';
-
-    $rootScope.$on('scanCompleted', function(e, data) {
-        console.log('HomeController got scanCompleted event: ' + data);
-        $scope.currentScanResult = data;
-    });
+  // stuff
 };
