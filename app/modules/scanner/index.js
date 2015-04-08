56fecad70fe17efa383ef5a8391ba7e291a6d84b
Kevin Smith <ksmithbaylor@gmail.com>
2015-04-08 00:26:01 -0400

    move scanner service into index for simplicity

--- app/modules/scanner/index.js
+++ app/modules/scanner/index.js
@@ -1,4 +1,27 @@
 'use strict';
 
 module.exports = require('angular').module('beekeep.scanner', ['ionic', 'ngCordova']);
-module.exports.factory('ScannerService', require('./ScannerService'));
+
+module.exports.factory('ScannerService', function($ionicPopup, $ionicPlatform) {
+  return {
+    scan: function (label, success, fail) {
+      if (window.cordova) {
+        var text = window.ionic.Platform.isAndroid() ? label : null;
+        cordova.plugins.barcodeScanner.scan(success, fail, text);
+      } else {
+        $ionicPopup.prompt({
+          title: 'Fake Scanner',
+          template: 'Enter a Hive Number',
+          inputType: 'text',
+          inputPlaceholder: '0000'
+        }).then(function (response) {
+          if (response || response === '') {
+            success({text: response});
+          } else {
+            success({cancelled: true});
+          }
+        });
+      }
+    }
+  }
+});
