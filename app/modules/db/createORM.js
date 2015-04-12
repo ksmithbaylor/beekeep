81c8a743b0925572fc6726104bdb66521c5d65ec
Kevin Smith <ksmithbaylor@gmail.com>
2015-04-12 14:27:29 -0400

    create initial db factory/orm

---
+++ app/modules/db/createORM.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = function (db, singular, plural, relations) {
+  return {
+    create: function* (object) {
+      return (yield db.rel.save(singular, object || {}))[plural][0];
+    },
+
+    find: function* (id, eager) {
+      var res = yield db.rel.find(singular, id);
+      if (eager) return res;
+      if (typeof id === 'object') return res[plural];
+      return res[plural][0];
+    },
+
+    all: function* (eager) {
+      var res = yield db.rel.find(singular);
+      if (eager) return res;
+      return res[plural];
+    },
+
+    destroy: function* (object) {
+      return (yield db.rel.del(singular, object)).deleted;
+    },
+
+    schema: {
+      singular: singular,
+      plural: plural,
+      relations: relations
+    }
+  };
+};
--- app/modules/db/index.js
+++ app/modules/db/index.js
@@ -1,39 +1,55 @@
 'use strict';
 
 var _ = require('lodash');
+var createORM = require('./createORM');
 
-// Load PouchDB, initiate plugins, and expose globally
-var PouchDB = require('pouchdb');
+var PouchDB = window.PouchDB = require('pouchdb');
 PouchDB.plugin(require('relational-pouch'));
 // TODO: add pouchdb-persist plugin for easier syncing
-window.PouchDB = PouchDB;
-
-// Load angular PouchDB wrapper
 require('angular-pouchdb');
 
 module.exports = require('angular').module('beekeep.db', ['pouchdb']);
 
 module.exports.factory('DB', function(pouchDB, pouchDecorators) {
-  var db = pouchDB('beekeep', {
-    adapter: 'websql'
+  var db = pouchDB('beekeep', { adapter: 'websql' });
+
+  var Queen = createORM(db, 'queen', 'queens', {
+    hive: {belongsTo: 'hive'}
+  });
+
+  var Hive = createORM(db, 'hive', 'hives', {
+    pallet: {belongsTo: 'pallet'},
+    queen: {belongsTo: 'queen'}
+  });
+
+  var Pallet = createORM(db, 'pallet', 'pallets', {
+    yard: {belongsTo: 'yard'},
+    hives: {hasMany: 'hive'}
+  });
+
+  var Yard = createORM(db, 'yard', 'yards', {
+    pallets: {hasMany: 'pallet'}
   });
 
   db.setSchema([
-    {singular: 'hive', plural: 'hives'}
+    Queen.schema,
+    Hive.schema,
+    Pallet.schema,
+    Yard.schema
   ]);
 
   _.each([
-    'save',
-    'find',
-    'del',
-    'putAttachment',
-    'getAttachment',
-    'removeAttachment',
-    'parseDocID',
-    'makeDocID'
+    'save', 'find', 'del', 'putAttachment', 'getAttachment',
+    'removeAttachment', 'parseDocID', 'makeDocID'
   ], function(fn) {
     db.rel[fn] = pouchDecorators.qify(db.rel[fn]);
   });
 
-  return db;
+  return {
+    db: db,
+    Queen: Queen,
+    Hive: Hive,
+    Pallet: Pallet,
+    Yard: Yard
+  };
 });
