cc0d49bf526c6c52fce4434847600d19c368b95c
Kevin Smith <ksmithbaylor@gmail.com>
2015-04-12 15:04:29 -0400

    add debug statments to all ORM methods

--- app/modules/db/createORM.js
+++ app/modules/db/createORM.js
@@ -3,29 +3,36 @@
 module.exports = function (db, singular, plural, relations) {
   return {
     create: function* (object) {
-      return (yield db.rel.save(singular, object || {}))[plural][0];
+      var created = yield db.rel.save(singular, object || {});
+      if (window.DEBUG) console.log('created a thing:', created);
+      return created[plural][0];
     },
 
     find: function* (id, eager) {
-      var res = yield db.rel.find(singular, id);
-      if (eager) return res;
-      if (typeof id === 'object') return res[plural];
-      return res[plural][0];
+      var found = yield db.rel.find(singular, id);
+      if (window.DEBUG) console.log('found a thing:', found);
+      if (eager) return found;
+      if (typeof id === 'object') return found[plural];
+      return found[plural][0];
     },
 
     all: function* (eager) {
-      var res = yield db.rel.find(singular);
-      if (eager) return res;
-      return res[plural];
+      var all = yield db.rel.find(singular);
+      if (window.DEBUG) console.log('got all things:', all);
+      if (eager) return all;
+      return all[plural];
     },
 
     first: function* () {
-      var res = yield db.rel.find(singular);
-      return res[plural] ? res[plural][0] : undefined;
-    }
+      var first = yield db.rel.find(singular);
+      if (window.DEBUG) console.log('got first thing:', first);
+      return first[plural] ? first[plural][0] : undefined;
+    },
 
     destroy: function* (object) {
-      return (yield db.rel.del(singular, object)).deleted;
+      var destroyed = yield db.rel.del(singular, object);
+      if (window.DEBUG) console.log('destroyed a thing:', destroyed);
+      return destroyed.deleted;
     },
 
     schema: {
