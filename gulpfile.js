d6a14fd7ed9516e45081512b2a0fea964f84e8f6
Kevin Smith <ksmithbaylor@gmail.com>
2015-04-12 22:27:10 -0400

    clean up gulpfile and package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,71 +1,84 @@
-var gulp       = require('gulp'),
-    sourcemaps = require('gulp-sourcemaps'),
-    browserify = require('browserify'),
-    bower      = require('bower'),
-    sass       = require('gulp-sass'),
-    rename     = require('gulp-rename'),
-    plumber    = require('gulp-plumber'),
-    tap        = require('gulp-tap'),
-    webserver  = require('gulp-webserver'),
-    fs         = require('fs'),
-    _          = require('lodash');
+var gulp       = require('gulp');
+var browserify = require('browserify');
+var watchify   = require('watchify');
+var source     = require('vinyl-source-stream');
+var sass       = require('gulp-sass');
+var rename     = require('gulp-rename');
+var plumber    = require('gulp-plumber');
+var server     = require('gulp-server-livereload');
+var chalk      = require('chalk');
+var _          = require('lodash');
 
 var inputDir  = './app/';
 var outputDir = './www/';
 
 var paths = {
-    sass       : [inputDir + 'style/app.scss'],
-    js         : [inputDir + '**/*.js'],
-    jsEntry    : inputDir  + 'app.js',
-    html       : [inputDir + '**/*.html'],
-    assets     : ['./assets/**/*'],
-    htmlDest   : outputDir,
-    libDest    : outputDir + 'lib/',
-    assetsDest : outputDir + 'assets/'
+  sass       : [inputDir + 'style/app.scss'],
+  js         : [inputDir + '**/*.js'],
+  jsEntry    : inputDir  + 'app.js',
+  html       : [inputDir + '**/*.html'],
+  assets     : ['./assets/**/*'],
+  htmlDest   : outputDir,
+  libDest    : outputDir + 'lib/',
+  assetsDest : outputDir + 'assets/'
 };
 
-gulp.task('compile', ['assets', 'html', 'sass', 'js'])
-gulp.task('default', ['compile', 'watch']);
+gulp.task('js', function (done) {
+  function bundle(b, done) {
+    b.bundle()
+      .on('error', function(err) {
+        console.log(chalk.red(err.message));
+        this.emit('end');
+      })
+      .pipe(source('app.bundle.js'))
+      .pipe(gulp.dest(outputDir + 'js/'))
+      .on('end', done || function(){});
+  };
 
-gulp.task('cordova', function() {
-    var cordovaString = 'module.exports={FAKE:true}\n';
-    fs.writeFileSync(outputDir + 'cordova.js', cordovaString);
-})
-
-gulp.task('js', ['cordova'], function () {
-    function bundler(file) {
-        file.contents = browserify(file.path).bundle();
-    }
-
-
-    gulp.src(paths.jsEntry, {read: false})
-        .pipe(plumber())
-        .pipe(tap(bundler))
-        .pipe(gulp.dest(outputDir + 'js/'));
+  var b = browserify(watchify.args);
+  var w = watchify(b);
+  w.on('update', function() { bundle(w); });
+  w.add(paths.jsEntry);
+  bundle(w, done);
 });
 
 gulp.task('sass', function(done) {
-    gulp.src(paths.sass)
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            errLogToConsole: true
-        }))
-        .pipe(sourcemaps.write())
-        .pipe(rename('app.bundle.css'))
-        .pipe(gulp.dest(outputDir + 'css/'))
-        .on('end', done);
+  gulp.src(paths.sass)
+    .pipe(plumber())
+    .pipe(sass({ errLogToConsole: true }))
+    .pipe(rename('app.bundle.css'))
+    .pipe(gulp.dest(outputDir + 'css/'))
+    .on('end', done);
 });
 
-_.each(['html', 'assets'], function(name) {
-    gulp.task(name, function() {
-        return gulp.src(paths[name]).pipe(gulp.dest(paths[name + 'Dest']));
-    });
+gulp.task('html', function(done) {
+  gulp.src(paths.html)
+    .pipe(gulp.dest(paths.htmlDest))
+    .on('end', done);
+});
+
+gulp.task('assets', function(done) {
+  gulp.src(paths.assets)
+    .pipe(gulp.dest(paths.assetsDest))
+    .on('end', done);
 });
 
 gulp.task('watch', function() {
-    var nonWatchPaths = ['jsEntry', 'htmlDest', 'libDest', 'assetsDest'];
-    _.forOwn(paths, function(path, name) {
-        if (_.contains(nonWatchPaths, name)) return;
-        gulp.watch(path, [name]);
-    });
+  gulp.watch(paths.sass, ['sass']);
+  gulp.watch(paths.html, ['html']);
+  gulp.watch(paths.assets, ['assets']);
+});
+
+gulp.task('default', [
+  'js',
+  'sass',
+  'html',
+  'assets',
+  'watch'
+], function() {
+  console.log(chalk.green('Starting server!'));
+  gulp.src(outputDir).pipe(server({
+    port: 8100,
+    livereload: true
+  }));
 });
