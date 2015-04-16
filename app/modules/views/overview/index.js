6b96a24ff696ac5ba3e26fb6dc043d1e4b70882f
Kevin Smith <ksmithbaylor@gmail.com>
2015-04-16 10:19:34 -0400

    change name of include for overview

--- app/modules/views/overview/index.js
+++ app/modules/views/overview/index.js
@@ -1,4 +1,4 @@
 'use strict';
 
 module.exports = require('angular').module('beekeep.overview', ['ionic']);
-module.exports.controller('OverviewController', require('./OverviewController'));
+module.exports.controller('OverviewController', require('./Controller'));
