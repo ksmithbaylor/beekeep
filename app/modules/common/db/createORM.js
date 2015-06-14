4c4acc6b47abf42f25acde95ec55e2fd2de0eb14
Kevin Smith <ksmithbaylor@gmail.com>
2015-06-13 23:50:40 -0400

    did the same thing for the DB module

--- app/modules/common/db/createORM.js
+++ app/modules/common/db/createORM.js
@@ -1,40 +1,45 @@
 'use strict';
 
 module.exports = function (db, singular, plural, relations) {
+  function* create(object) {
+    var created = yield db.rel.save(singular, object || {});
+    if (window.DEBUG) console.log('created a ' + this.schema.singular + ':', created);
+    return created[plural][0];
+  }
+
+  function* find(id, eager) {
+    var found = yield db.rel.find(singular, id);
+    if (window.DEBUG) console.log('found a ' + this.schema.singular + ':', found);
+    if (eager) return found;
+    if (typeof id === 'object') return found[plural];
+    return found[plural][0];
+  }
+
+  function* all(eager) {
+    var all = yield db.rel.find(singular);
+    if (window.DEBUG) console.log('got all ' + this.schema.plural + ':', all);
+    if (eager) return all;
+    return all[plural];
+  }
+
+  function* first() {
+    var first = yield db.rel.find(singular);
+    if (window.DEBUG) console.log('got first ' + this.schema.singular + ':', first);
+    return first[plural] ? first[plural][0] : undefined;
+  }
+
+  function* destroy(object) {
+    var destroyed = yield db.rel.del(singular, object);
+    if (window.DEBUG) console.log('destroyed a ' + this.schema.singular + ':', destroyed);
+    return destroyed.deleted;
+  }
+
   return {
-    create: function* (object) {
-      var created = yield db.rel.save(singular, object || {});
-      if (window.DEBUG) console.log('created a ' + this.schema.singular + ':', created);
-      return created[plural][0];
-    },
-
-    find: function* (id, eager) {
-      var found = yield db.rel.find(singular, id);
-      if (window.DEBUG) console.log('found a ' + this.schema.singular + ':', found);
-      if (eager) return found;
-      if (typeof id === 'object') return found[plural];
-      return found[plural][0];
-    },
-
-    all: function* (eager) {
-      var all = yield db.rel.find(singular);
-      if (window.DEBUG) console.log('got all ' + this.schema.plural + ':', all);
-      if (eager) return all;
-      return all[plural];
-    },
-
-    first: function* () {
-      var first = yield db.rel.find(singular);
-      if (window.DEBUG) console.log('got first ' + this.schema.singular + ':', first);
-      return first[plural] ? first[plural][0] : undefined;
-    },
-
-    destroy: function* (object) {
-      var destroyed = yield db.rel.del(singular, object);
-      if (window.DEBUG) console.log('destroyed a ' + this.schema.singular + ':', destroyed);
-      return destroyed.deleted;
-    },
-
+    create: create,
+    find: find,
+    all: all,
+    first: first,
+    destroy: destroy,
     schema: {
       singular: singular,
       plural: plural,
