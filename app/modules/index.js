dcf4ba03f731d1b6f5c9ebcccc5001819e54eeab
Kevin Smith <ksmithbaylor@gmail.com>
2015-06-13 23:12:09 -0400

    added space between logical separation of modules

--- app/modules/index.js
+++ app/modules/index.js
@@ -4,11 +4,13 @@ var _ = require('lodash');
 
 module.exports = _.map([
     require('./views/navigation'),
+
     require('./views/home'),
     require('./views/browse'),
     require('./views/activity'),
     require('./views/todo'),
     require('./views/sandbox'),
+
     require('./common/scanner'),
     require('./common/db')
 ], function(x) { return x.name; });
