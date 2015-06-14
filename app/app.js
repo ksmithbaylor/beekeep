fa0c9d46408d775b26c13f510641fffdeba4b18e
Kevin Smith <ksmithbaylor@gmail.com>
2015-06-13 22:54:09 -0400

    add some comments to clarify entry point

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Dependencies
 var _ = require('lodash');
 var co = require('co');
 var angular = require('angular');
@@ -8,23 +9,28 @@ var angular = require('angular');
 require('ionic-angular');
 require('ng-cordova');
 
+// Toggles for development
 window.DEBUG = true;
 var resetDatabase = false;
 
-// Declare the main module
+// Declare the main angular module
 var app = angular.module('beekeep', _.flatten([
     'ionic',
     require('./modules') // load all modules
 ]));
 
+// Ionic config and routes
 app.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+  // Style and behavior defaults
   $ionicConfigProvider.tabs.position('bottom');
   $ionicConfigProvider.navBar.alignTitle('center');
   $ionicConfigProvider.views.transition('ios');
 
+  // Load routes
   require('./routes.js')($stateProvider, $urlRouterProvider);
 });
 
+// Development reset database
 if (resetDatabase) {
   app.run(function(DB, $rootScope) {
     return co(function* () {
@@ -43,10 +49,12 @@ if (resetDatabase) {
   });
 }
 
-// App initialization module
+// Initialize the app
 app.run(function($rootScope, DB, $ionicPlatform) {
+  // Holds things we need across the whole app
   $rootScope.data = {};
 
+  // Helper functions for updating the models
   $rootScope.update = function(thing) {
     return co(function* () {
       $rootScope.data[thing.toLowerCase() + 's'] = yield DB[thing].all();
@@ -54,14 +62,17 @@ app.run(function($rootScope, DB, $ionicPlatform) {
     });
   };
 
+  // Update each model
   _.each(['Yard', 'Pallet', 'Hive'], function(thing) {
     return $rootScope.update(thing);
   });
 
+  // More default styling
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
+
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
