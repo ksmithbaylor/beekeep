f9a01d49d29ff14a3370317ee3cd032f8a62dcc0
Kevin Smith <ksmithbaylor@gmail.com>
2015-07-15 20:41:38 -0400

    deleting a bee yard now works as expected

--- app/modules/views/yards/YardDetailController.js
+++ app/modules/views/yards/YardDetailController.js
@@ -3,7 +3,7 @@
 var co = require('co');
 var reFind = require('../../common/util/reFind');
 
-module.exports = function($scope, $rootScope, $stateParams, $ionicHistory, DB) {
+module.exports = function($scope, $rootScope, $stateParams, $ionicHistory, $ionicPopup, DB) {
   reFind($scope, $stateParams, DB, 'app.yardDetail', 'Yard');
 
   $scope.updateYard = function() {
@@ -13,4 +13,22 @@ module.exports = function($scope, $rootScope, $stateParams, $ionicHistory, DB) {
       $ionicHistory.goBack();
     });
   };
+
+  $scope.deleteYard = function() {
+    co(function *() {
+      console.log('trying to delete');
+      $ionicPopup.confirm({
+        title: 'Delete Yard',
+        template: 'Are you sure you want to permanently delete this Bee Yard?'
+      }).then(function (response) {
+        if (response) {
+          co(function *() {
+            yield DB.Yard.destroy($scope.yard);
+            $rootScope.update('Yard');
+            $ionicHistory.goBack();
+          });
+        }
+      })
+    })
+  }
 };
